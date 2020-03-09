20487e06e4d428c2369c139e7e9c569fb3fc8b83
dim42 <dimt777@gmail.com>
2020-03-09 20:46:30 +0300

    Update libs; cleanup; RunTestOnContext

--- src/test/java/flat/viewer/FlatViewServiceTest.kt
+++ src/test/java/flat/viewer/FlatViewServiceTest.kt
@@ -1,12 +1,15 @@
 package flat.viewer
 
-import org.hamcrest.CoreMatchers
-import org.junit.Assert
+import flat.viewer.Result.*
+import flat.viewer.SlotState.*
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.ArgumentMatchers
 import org.mockito.Mockito
+import org.mockito.Mockito.*
 import org.mockito.Spy
 import org.mockito.junit.MockitoJUnitRunner
 import java.time.LocalDateTime
@@ -16,9 +19,10 @@ import java.util.*
 class FlatViewServiceTest {
     @Spy
     private val notificationService: NotificationService = NotificationServiceImpl()
-    private var flatViewService: FlatViewService? = null
+    private lateinit var flatViewService: FlatViewService
     private lateinit var flatToCurrentTenant: MutableMap<Int?, Int?>
     private lateinit var flatViewToNewTenant: MutableMap<FlatViewSlot?, ViewSlot?>
+
     @Before
     fun setUp() {
         flatViewService = FlatViewService(notificationService)
@@ -32,16 +36,22 @@ class FlatViewServiceTest {
         val tenant2Id = 12
         val flatId = 45
         val start = LocalDateTime.now().plusDays(5)
-        var result = flatViewService!!.tryReserve(flatId, start, tenant2Id, flatViewToNewTenant)
-        Assert.assertThat(result, CoreMatchers.equalTo(Result.Ok))
-        Mockito.verify(notificationService, Mockito.never()).notifyTenant(ArgumentMatchers.any(Int::class.java), any(), any())
-        flatViewService!!.cancel(flatId, start, tenant2Id, flatViewToNewTenant)
-        flatViewService!!.rent(flatId, tenant1Id, flatToCurrentTenant)
-        flatViewService!!.tryReserve(flatId, start, tenant2Id, flatViewToNewTenant)
-        Mockito.verify(notificationService).notifyTenant(tenant1Id, start, SlotState.RESERVING)
-        result = flatViewService!!.approve(flatId, start, tenant2Id, flatToCurrentTenant, flatViewToNewTenant)
-        Assert.assertThat(result, CoreMatchers.equalTo(Result.NotCurrent))
-        Mockito.verify(notificationService).notifyTenant(ArgumentMatchers.any(Int::class.java), any(), any())
+
+        var result = flatViewService.tryReserve(flatId, start, tenant2Id, flatViewToNewTenant)
+
+        assertThat(result, equalTo(Ok))
+        verify(notificationService, never()).notifyTenant(ArgumentMatchers.any(Int::class.java), any(), any())
+
+        flatViewService.cancel(flatId, start, tenant2Id, flatViewToNewTenant)
+        flatViewService.rent(flatId, tenant1Id, flatToCurrentTenant)
+        flatViewService.tryReserve(flatId, start, tenant2Id, flatViewToNewTenant)
+
+        verify(notificationService).notifyTenant(tenant1Id, start, RESERVING)
+
+        result = flatViewService.approve(flatId, start, tenant2Id, flatToCurrentTenant, flatViewToNewTenant)
+
+        assertThat(result, equalTo(NotCurrent))
+        verify(notificationService).notifyTenant(ArgumentMatchers.any(Int::class.java), any(), any())
     }
 
     private fun <T> any(): T {
@@ -55,17 +65,22 @@ class FlatViewServiceTest {
         val tenant3Id = 13
         val flatId = 45
         val start = LocalDateTime.now().plusDays(5)
+
         var result: Result?
-        result = flatViewService!!.rent(flatId, tenant1Id, flatToCurrentTenant)
-        Assert.assertThat(result, CoreMatchers.equalTo(Result.Ok))
-        result = flatViewService!!.tryReserve(flatId, start, tenant2Id, flatViewToNewTenant)
-        Assert.assertThat(result, CoreMatchers.equalTo(Result.Ok))
-        Mockito.verify(notificationService).notifyTenant(tenant1Id, start, SlotState.RESERVING)
-        result = flatViewService!!.tryReserve(flatId, start, tenant3Id, flatViewToNewTenant)
-        Assert.assertThat(result, CoreMatchers.equalTo(Result.Occupied))
-        result = flatViewService!!.approve(flatId, start, tenant1Id, flatToCurrentTenant, flatViewToNewTenant)
-        Assert.assertThat(result, CoreMatchers.equalTo(Result.Ok))
-        Mockito.verify(notificationService).notifyTenant(tenant2Id, start, SlotState.APPROVED)
+        result = flatViewService.rent(flatId, tenant1Id, flatToCurrentTenant)
+        assertThat(result, equalTo(Ok))
+        result = flatViewService.tryReserve(flatId, start, tenant2Id, flatViewToNewTenant)
+
+        assertThat(result, equalTo(Ok))
+        verify(notificationService).notifyTenant(tenant1Id, start, RESERVING)
+
+        result = flatViewService.tryReserve(flatId, start, tenant3Id, flatViewToNewTenant)
+        assertThat(result, equalTo(Occupied))
+
+        result = flatViewService.approve(flatId, start, tenant1Id, flatToCurrentTenant, flatViewToNewTenant)
+
+        assertThat(result, equalTo(Ok))
+        verify(notificationService).notifyTenant(tenant2Id, start, APPROVED)
     }
 
     @Test
@@ -75,15 +90,19 @@ class FlatViewServiceTest {
         val tenant3Id = 13
         val flatId = 45
         val start = LocalDateTime.now().plusDays(5)
+
         var result: Result
-        result = flatViewService!!.rent(flatId, tenant1Id, flatToCurrentTenant)
-        Assert.assertThat(result, CoreMatchers.equalTo(Result.Ok))
-        flatViewService!!.tryReserve(flatId, start, tenant2Id, flatViewToNewTenant)
-        result = flatViewService!!.cancel(flatId, start, tenant2Id, flatViewToNewTenant)
-        Assert.assertThat(result, CoreMatchers.equalTo(Result.Ok))
-        Mockito.verify(notificationService).notifyTenant(tenant1Id, start, SlotState.CANCELED)
-        result = flatViewService!!.tryReserve(flatId, start, tenant3Id, flatViewToNewTenant)!!
-        Assert.assertThat(result, CoreMatchers.equalTo(Result.Ok))
+        result = flatViewService.rent(flatId, tenant1Id, flatToCurrentTenant)
+        assertThat(result, equalTo(Ok))
+        flatViewService.tryReserve(flatId, start, tenant2Id, flatViewToNewTenant)
+
+        result = flatViewService.cancel(flatId, start, tenant2Id, flatViewToNewTenant)
+
+        assertThat(result, equalTo(Ok))
+        verify(notificationService).notifyTenant(tenant1Id, start, CANCELED)
+
+        result = flatViewService.tryReserve(flatId, start, tenant3Id, flatViewToNewTenant)!!
+        assertThat(result, equalTo(Ok))
     }
 
     @Test
@@ -94,14 +113,17 @@ class FlatViewServiceTest {
         val flatId = 45
         val start = LocalDateTime.now().plusDays(5)
         var result: Result
-        flatViewService!!.rent(flatId, tenant1Id, flatToCurrentTenant)
-        flatViewService!!.tryReserve(flatId, start, tenant2Id, flatViewToNewTenant)
-        Mockito.verify(notificationService).notifyTenant(tenant1Id, start, SlotState.RESERVING)
-        result = flatViewService!!.reject(flatId, start, tenant1Id, flatToCurrentTenant, flatViewToNewTenant)
-        Assert.assertThat(result, CoreMatchers.equalTo(Result.Ok))
-        Mockito.verify(notificationService).notifyTenant(tenant2Id, start, SlotState.REJECTED)
-        result = flatViewService!!.tryReserve(flatId, start, tenant3Id, flatViewToNewTenant)!!
-        Assert.assertThat(result, CoreMatchers.equalTo(Result.Rejected))
-        Mockito.verify(notificationService, Mockito.times(2)).notifyTenant(ArgumentMatchers.any(Int::class.java), any(), any())
+        flatViewService.rent(flatId, tenant1Id, flatToCurrentTenant)
+        flatViewService.tryReserve(flatId, start, tenant2Id, flatViewToNewTenant)
+        verify(notificationService).notifyTenant(tenant1Id, start, RESERVING)
+        result = flatViewService.reject(flatId, start, tenant1Id, flatToCurrentTenant, flatViewToNewTenant)
+
+        assertThat(result, equalTo(Ok))
+        verify(notificationService).notifyTenant(tenant2Id, start, REJECTED)
+
+        result = flatViewService.tryReserve(flatId, start, tenant3Id, flatViewToNewTenant)!!
+
+        assertThat(result, equalTo(Rejected))
+        verify(notificationService, times(2)).notifyTenant(ArgumentMatchers.any(Int::class.java), any(), any())
     }
 }
--- src/test/java/flat/viewer/web/IntegrationTest.kt
+++ src/test/java/flat/viewer/web/IntegrationTest.kt
@@ -1,69 +1,75 @@
 package flat.viewer.web
 
-import com.jayway.restassured.RestAssured
-import com.jayway.restassured.http.ContentType
-import flat.viewer.web.FlatViewController
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType.JSON
 import io.vertx.core.DeploymentOptions
 import io.vertx.core.Vertx
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.unit.TestContext
+import io.vertx.ext.unit.junit.RunTestOnContext
 import io.vertx.ext.unit.junit.VertxUnitRunner
 import org.hamcrest.CoreMatchers
 import org.junit.After
-import org.junit.Assert
+import org.junit.Assert.assertThat
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
 import java.time.LocalDate
 
+
 @RunWith(VertxUnitRunner::class)
 class IntegrationTest {
-    private var vertx: Vertx? = null
+    private lateinit var vertx: Vertx
+
+    @get:Rule
+    var runTestOnContextRule = RunTestOnContext()
+
     @Before
     @Throws(IOException::class)
     fun setUp(context: TestContext) {
-        vertx = Vertx.vertx()
+        vertx = runTestOnContextRule.vertx()
         val options = DeploymentOptions()
                 .setConfig(JsonObject().put("http.port", 8080))
-        vertx!!.deployVerticle(FlatViewController::class.java.name, options, context.asyncAssertSuccess())
+        vertx.deployVerticle(FlatViewController::class.java.name, options, context.asyncAssertSuccess())
     }
 
     @After
     fun tearDown(context: TestContext) {
-        vertx!!.close(context.asyncAssertSuccess())
+        vertx.close(context.asyncAssertSuccess())
     }
 
     @Test
     fun testReserve() {
-        val result = RestAssured.given()
-                .body("{\"t_id\":11, \"f_id\":45}").contentType(ContentType.JSON).accept(ContentType.JSON)
+        val result = given()
+                .body("{\"t_id\":11, \"f_id\":45}").contentType(JSON).accept(JSON)
                 .post("/rent")
                 .thenReturn().asString()
-        Assert.assertThat(result, CoreMatchers.equalTo("{\"result\":\"Ok\"}"))
+        assertThat(result, CoreMatchers.equalTo("{\"result\":\"Ok\"}"))
         val start = LocalDate.now().plusDays(2).format(FlatViewHandler.DATE_FORMATTER)
-        RestAssured.given()
-                .body(String.format("{\"t_id\":21, \"f_id\":45, \"date\":\"%s\", \"time\":\"12:20\", \"duration\":20}", start)).contentType(ContentType.JSON).accept(ContentType.JSON)
+        given()
+                .body(String.format("{\"t_id\":21, \"f_id\":45, \"date\":\"%s\", \"time\":\"12:20\", \"duration\":20}", start)).contentType(JSON).accept(JSON)
                 .post("/reserve")
                 .then().assertThat().body(CoreMatchers.equalTo("{\"result\":\"Ok\"}"))
-        RestAssured.given()
-                .body(String.format("{\"t_id\":22, \"f_id\":45, \"date\":\"%s\", \"time\":\"12:20\", \"duration\":20}", start)).contentType(ContentType.JSON).accept(ContentType.JSON)
+        given()
+                .body(String.format("{\"t_id\":22, \"f_id\":45, \"date\":\"%s\", \"time\":\"12:20\", \"duration\":20}", start)).contentType(JSON).accept(JSON)
                 .post("/reserve")
                 .then().assertThat().body(CoreMatchers.equalTo("{\"result\":\"Occupied\"}"))
-        RestAssured.given()
-                .body(String.format("{\"t_id\":11, \"f_id\":45, \"date\":\"%s\", \"time\":\"12:20\", \"duration\":20}", start)).contentType(ContentType.JSON).accept(ContentType.JSON)
+        given()
+                .body(String.format("{\"t_id\":11, \"f_id\":45, \"date\":\"%s\", \"time\":\"12:20\", \"duration\":20}", start)).contentType(JSON).accept(JSON)
                 .post("/approve")
                 .then().assertThat().body(CoreMatchers.equalTo("{\"result\":\"Ok\"}"))
-        RestAssured.given()
-                .body(String.format("{\"t_id\":21, \"f_id\":45,\"date\":\"%s\", \"time\":\"12:20\", \"duration\":20}", start)).contentType(ContentType.JSON).accept(ContentType.JSON)
+        given()
+                .body(String.format("{\"t_id\":21, \"f_id\":45,\"date\":\"%s\", \"time\":\"12:20\", \"duration\":20}", start)).contentType(JSON).accept(JSON)
                 .post("/cancel")
                 .then().assertThat().body(CoreMatchers.equalTo("{\"result\":\"Ok\"}"))
-        RestAssured.given()
-                .body(String.format("{\"t_id\":22, \"f_id\":45, \"date\":\"%s\", \"time\":\"12:20\", \"duration\":20}", start)).contentType(ContentType.JSON).accept(ContentType.JSON)
+        given()
+                .body(String.format("{\"t_id\":22, \"f_id\":45, \"date\":\"%s\", \"time\":\"12:20\", \"duration\":20}", start)).contentType(JSON).accept(JSON)
                 .post("/reserve")
                 .then().assertThat().body(CoreMatchers.equalTo("{\"result\":\"Ok\"}"))
-        RestAssured.given()
-                .body(String.format("{\"t_id\":11, \"f_id\":45, \"date\":\"%s\", \"time\":\"12:20\", \"duration\":20}", start)).contentType(ContentType.JSON).accept(ContentType.JSON)
+        given()
+                .body(String.format("{\"t_id\":11, \"f_id\":45, \"date\":\"%s\", \"time\":\"12:20\", \"duration\":20}", start)).contentType(JSON).accept(JSON)
                 .post("/reject")
                 .then().assertThat().body(CoreMatchers.equalTo("{\"result\":\"Ok\"}"))
     }
